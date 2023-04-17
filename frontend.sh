9d25fd9139ce2a673b0752107817414c79b51d11
suji <borra.sujani@gmail.com>
2023-04-17 12:38:18 -0400

    upadated localhost in all

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,10 @@
 echo -e "\e[36m>>>>>>>> installing nginx <<<<<<<<<<< \e[0m"
 yum install nginx -y
 
+echo -e "\e[36m>>>>>>>> restarting nginxx <<<<<<<<<<< \e[0m"
+systemctl enable nginx
+systemctl start nginx
+
 echo -e "\e[36m>>>>>>>> configuring file <<<<<<<<<<< \e[0m"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
@@ -17,4 +21,3 @@ unzip /tmp/frontend.zip
 echo -e "\e[36m>>>>>>>> restarting  nginx <<<<<<<<<<< \e[0m"
 
 systemctl restart nginx
-systemctl enable nginx
