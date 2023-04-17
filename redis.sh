eb1aa3d226a639461582191407af2115166fbd15
suji <borra.sujani@gmail.com>
2023-04-16 21:54:45 -0400

    redis added 127.0.0.1

--- redis.sh
+++ redis.sh
@@ -2,6 +2,6 @@ yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
-#have to update the redis 127.0.0.1 to 0.0.0.0
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 systemctl enable redis
 systemctl start redis
