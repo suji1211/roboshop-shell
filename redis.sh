19149221737b80dcf280bf7bdfc2a58db9fb8d85
suji <borra.sujani@gmail.com>
2023-04-17 17:54:55 -0400

    mongo file directory changes

--- redis.sh
+++ redis.sh
@@ -2,7 +2,7 @@ echo -e "/e[36m>>>>>> installing redis repo<<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
 
-echo -e "/e[36m>>>>>> install redis repo<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>> install redis repo<<<<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
