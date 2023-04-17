af67c6afcaa0f16a7ac1a5f3b4b8bd10ff2f50f6
suji <borra.sujani@gmail.com>
2023-04-17 12:34:06 -0400

    upadated localhost in all

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,20 @@
+echo -e "\e[36m>>>>>>>> installing nginx <<<<<<<<<<< \e[0m"
 yum install nginx -y
+
+echo -e "\e[36m>>>>>>>> configuring file <<<<<<<<<<< \e[0m"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[36m>>>>>>>> Removing Nginx data <<<<<<<<<<< \e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[36m>>>>>>>> downloading file <<<<<<<<<<< \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
+
+echo -e "\e[36m>>>>>>>> unzip the file <<<<<<<<<<< \e[0m"
 unzip /tmp/frontend.zip
+
+echo -e "\e[36m>>>>>>>> restarting  nginx <<<<<<<<<<< \e[0m"
+
 systemctl restart nginx
 systemctl enable nginx
