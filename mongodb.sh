dcd46929052f79267409123354337e85508f7a9a
suji <borra.sujani@gmail.com>
2023-04-17 13:02:55 -0400

    mongodb script

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,13 @@
+echo -e "\e[36m>>>>>>>>> mongo config file <<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>> installing mongo file <<<<<<<<<<< \e[0m"
 yum install mongodb-org -y
-sed -i -e 's|127.0.0.1|0.0.0.0|' mongo.repo
+
+echo -e "\e[36m>>>>>>>> changing 127.0.0.1 to 0.0.0.0 <<<<<<<<<<< \e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
+echo -e "\e[36m>>>>>>>> restarting mongo file <<<<<<<<<<< \e[0m"
 systemctl enable mongod
 systemctl start mongod
 
