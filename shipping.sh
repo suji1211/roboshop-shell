30a1be7729b953ab57771ce65d54be817991be35
suji <borra.sujani@gmail.com>
2023-04-18 17:47:53 -0400

    removed directory before adding

--- shipping.sh
+++ shipping.sh
@@ -5,6 +5,7 @@ echo -e "\e[36m>>>>>>Adding application user<<<<<<<\e[0m"
 useradd roboshop
 
 echo -e "\e[36m>>>>>>creating DIRECTORY<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
 
 echo -e "\e[36m>>>>>>downloading shipping file<<<<<<<<\e[0m"
@@ -16,9 +17,6 @@ cd /app
 echo -e "\e[36m>>>>>>Extacting the shipping file<<<<<<<<\e[0m"
 unzip /tmp/shipping.zip
 
-echo -e "\e[36m>>>>>>CHANGING TO APP DIRECTORY<<<<<<<<\e[0m"
-cd /app
-
 echo -e "\e[36m>>>>>>cleaning maven package<<<<<<<<\e[0m"
 mvn clean package
 
@@ -26,14 +24,8 @@ echo -e "\e[36m>>>>>>Cmoving file <<<<<<<<\e[0m"
 mv target/shipping-1.0.jar shipping.jar
 
 echo -e "\e[36m>>>>>>copying file<<<<<<<<\e[0m"
-cp shipping.service /etc/systemd/system/shipping.service
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
 
-echo -e "\e[36m>>>>>>Creloading the service<<<<<<<<\e[0m"
-systemctl daemon-reload
-
-echo -e "\e[36m>>>>>>starting and enabling service<<<<<<<<\e[0m"
-systemctl enable shipping
-systemctl start shipping
 
 echo -e "\e[36m>>>>>>installing mysql<<<<<<<<\e[0m"
 yum install mysql -y
@@ -42,5 +34,10 @@ echo -e "\e[36m>>>>>>calling the service<<<<<<<<\e[0m"
 mysql -h mysql-dev.sujianilsrisriyaan.online -uroot -pRoboShop@1 /app/schema/shipping.sql
 
 echo -e "\e[36m>>>>>>restarting the service<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>Creloading the service<<<<<<<<\e[0m"
+systemctl daemon-reload
+
+echo -e "\e[36m>>>>>>starting and enabling service<<<<<<<<\e[0m"
+systemctl enable shipping
 systemctl restart shipping
 
