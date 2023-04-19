0f7ce75563a8f68374c56671805878e50fc1d813
suji <borra.sujani@gmail.com>
2023-04-19 11:09:43 -0400

    removed directory before adding

--- mysql.sh
+++ mysql.sh
@@ -11,7 +11,7 @@ echo -e "\e[36m>>>>>> enabling my sql<<<<<<<\e[0m"
 systemctl enable mysqld
 
 echo -e "\e[36m>>>>>>starting the service<<<<<<<\e[0m"
-systemctl start mysqld
+systemctl restart mysqld
 
 echo -e "\e[36m>>>>>>resetting mysql password<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
