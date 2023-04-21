2b45d243daf51c93b47f938a4417200249cfe6ec
suji <borra.sujani@gmail.com>
2023-04-21 17:18:08 -0400

    stat check

--- payment.sh
+++ payment.sh
@@ -1,9 +1,9 @@
 script=$(realpath "0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-rabbitmq_userapp_password=$1
+rabbitmq_appuser_password=$1
 
-if [ -z "$rabbitmq_userapp_password" ]; then
+if [ -z "$rabbitmq_appuser_password" ]; then
   echo Input roboshop appuser password Missing
   exit
 fi
