b097811ea3cfa5cb42db049beece2b9af2037c85
suji <borra.sujani@gmail.com>
2023-04-21 15:54:52 -0400

    stat check

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,12 @@
 script=$(realpath "0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+mysql_root_password=$1
 
-#if [ -z "$mysql_root_password" ]; then
- # echo Input MySQL Root password Missing
- # exit
-#fi
+if [ -z "$mysql_root_password" ]; then
+  echo Input MySQL Root password Missing
+   exit
+fi
 
 func_print_head "disabling the mysql"
 dnf module disable mysql -y &>>$log_file
