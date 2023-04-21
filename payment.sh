c215a467fff0ddb8dc276a84846759087bbff629
suji <borra.sujani@gmail.com>
2023-04-21 16:38:31 -0400

    stat check

--- payment.sh
+++ payment.sh
@@ -1,7 +1,7 @@
 script=$(realpath "0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-rabbitmq_appuser_password=$1
+mysql_root_password=$1
 
 if [ -z "mysql_root_password" ]; then
   echo Input MySQL Root password Missing
