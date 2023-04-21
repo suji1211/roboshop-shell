025f7e92f2414a6f09da245ef26a729d8e57a30c
suji <borra.sujani@gmail.com>
2023-04-21 17:50:14 -0400

    stat check

--- dispatch.sh
+++ dispatch.sh
@@ -4,13 +4,13 @@ source ${script_path}/common.sh
 
 component=dispatch
 func_print_head "installing golang"
-yum install golang -y
+yum install golang -y &>>$log_file
 
 func_app_prereq
 
 func_print_head "dispatching commands"
-go mod init dispatch
-go get
-go build
+go mod init dispatch &>>$log_file
+go get &>>$log_file
+go build &>>$log_file
 
 func_systemd_setup
