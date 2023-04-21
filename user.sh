95d6bd20c46fc6c5a3960ff45fe0ca09234641d1
suji <borra.sujani@gmail.com>
2023-04-20 20:50:49 -0400

    stat check

--- user.sh
+++ user.sh
@@ -2,12 +2,6 @@ script=$(realpath "0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-
-if [ -z "mysql_root_password" ]; then
-  echo Input MySQL Root password Missing
-  exit
-fi
-
 component=user
 schema_setup=mongo
 func_nodejs
