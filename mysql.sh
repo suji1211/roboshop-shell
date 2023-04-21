52a01d1e5dffbe64d24a6c19ae3d828e6ce3ce86
suji <borra.sujani@gmail.com>
2023-04-21 15:42:47 -0400

    stat check

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@ script=$(realpath "0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-if [ -z "mysql_root_password" ]; then
+if [ -z "$mysql_root_password" ]; then
   echo Input MySQL Root password Missing
   exit
 fi
