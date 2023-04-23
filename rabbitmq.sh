614a717aaf2947d9705adb0c96d5d51175a6a226
suji <borra.sujani@gmail.com>
2023-04-23 19:55:10 -0400

    stat check

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,32 +1,29 @@
 script=$(realpath "0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-rabbitmq_appuser_password=$1
 
-if [ -z "$rabbitmq_appuser_password" ]; then
-  echo Input Roboshop Appuser password Missing
-  exit
-fi
-
-func_print_head "downloading rabbitmq repos"
+func_print_head "download rabbitmq and erlang supporting file"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log_file
 func_stat_check $?
 
-func_print_head "setup Rabbitmq repos"
+func_print_head "install erlang"
+yum install erlang -y &>>$log_file
+func_stat_check $?
+
+func_print_head "download rabbitmq and erlang supporting file"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
 func_stat_check $?
 
-func_print_head "Installing erlang & RabbitMQ "
-yum install erlang  rabbitmq-server -y &>>$log_file
+func_print_head "install rabbitmq supporting file"
+yum install rabbitmq-server -y &>>$log_file
 func_stat_check $?
 
-
-func_print_head  "Restart rabbitmq server"
+func_print_head "Enable and restart server"
 systemctl enable rabbitmq-server &>>$log_file
-systemctl restart rabbitmq-server &>>$log_file
+systemctl start rabbitmq-server &>>$log_file
 func_stat_check $?
 
-func_print_head  "Add Application user in Rabbitmq"
-rabbitmqctl add_user roboshop ${rabbitmq_appuser_password} &>>$log_file
+func_print_head "Setting username and password"
+rabbitmqctl add_user roboshop roboshop123 &>>$log_file
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
 func_stat_check $?
