cc9ce7d8f1906a3e99842dfdf1cc11fa601ca158
suji <borra.sujani@gmail.com>
2023-04-24 08:30:41 -0400

    stat check

--- payment.sh
+++ payment.sh
@@ -1,29 +1,13 @@
 script=$(realpath "0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
 
-func_print_head "installing python36"
-yum install python36 gcc python3-devel -y
 
-func_print_head "adding application user"
-useradd roboshop
+if [ -z "$mysql_root_password" ]; then
+  echo Input MySQL Root password Missing
+   exit
+fi
 
-func_print_head "creating directory"
-rm -rf /app
-mkdir /app
-
-func_print_head "downloading and unzipping the content"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
-cd /app
-unzip /tmp/payment.zip
-
-func_print_head "Installing dependencies"
-cd /app
-pip3.6 install -r requirements.txt
-
-func_print_head "Reloading the service"
-systemctl daemon-reload
-
-func_print_head "Restarting the service"
-systemctl enable payment
-systemctl start payment
+component=payment
+func_python
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,21 +1,24 @@
 script=$(realpath "0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
+
+
+if [ -z "$mysql_root_password" ]; then
+  echo Input MySQL Root password Missing
+   exit
+fi
 
 func_print_head "download rabbitmq and erlang supporting file"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log_file
-func_stat_check $?
-
-func_print_head "install erlang"
-yum install erlang -y &>>$log_file
 func_stat_check $?
 
 func_print_head "download rabbitmq and erlang supporting file"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
 func_stat_check $?
 
-func_print_head "install rabbitmq supporting file"
-yum install rabbitmq-server -y &>>$log_file
+func_print_head "install Erlang and rabbitmq supporting file"
+yum install erlang rabbitmq-server -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Enable and restart server"
@@ -24,6 +27,6 @@ systemctl start rabbitmq-server &>>$log_file
 func_stat_check $?
 
 func_print_head "Setting username and password"
-rabbitmqctl add_user roboshop roboshop123 &>>$log_file
+rabbitmqctl add_user roboshop ${rabbitmq_appuser_password} &>>$log_file
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
 func_stat_check $?
